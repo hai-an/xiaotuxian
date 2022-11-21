212dd27dcf3ae8d58898226d3168cf1c5ef034e4
hai-an <1207783846@qq.com>
2022-11-21 18:49:26 +0800

    请求工具-请求函数封装

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <nav>
-  App
+  App {{$store.state.user.profile.nickname}}<button @click="$store.commit('user/setUser',{nickname:'Vue3'})">点击姓名</button>
   </nav>
 </template>
-
+<script>
+// import {  } from "vuex";
+</script>
 <style lang="less">
 
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,10 @@
 import { createStore } from 'vuex'
-
+// import { createPersistedstate } from 'vuex-persistedstate' // 引入 vuex 持久化包
+import createPersistedstate from 'vuex-persistedstate'
+// 引入三个子模块
+import user from './modules/user'
+import cart from './modules/cart'
+import category from './modules/category'
 // 创建vuex仓库并导出
 export default createStore({
   state: {
@@ -13,8 +18,17 @@ export default createStore({
   },
   modules: {
     // 分模块
+    user,
+    cart,
+    category
   },
   getters: {
     // vuex的计算属性
-  }
+  },
+  plugins: [
+    createPersistedstate({
+      key: 'erabbit-client-pc-store',
+      paths: ['user', 'cart']
+    })
+  ]
 })
