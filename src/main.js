473a59a244bfb0fa3a33f04273c245f60f7f4e13
hai-an <1207783846@qq.com>
2022-11-21 20:25:04 +0800

    重置样式使用normalize.css，项目公用样式common.less

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import 'normalize.css' // 重置样式的库normalize
+import '@/assets/styles/common.less' // 导入公共样式
 createApp(App).use(store).use(router).mount('#app')
