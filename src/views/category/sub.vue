2d9abe8ae9d7b3acacd843d8caf5d21166fa2129
hai-an <1207783846@qq.com>
2022-11-25 20:30:53 +0800

    二级类目-结果区-数据加载

--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -5,28 +5,81 @@
       <SubBread />
       <!-- 商品筛选区 -->
       <SubFilter />
-         <!-- 结果区域 -->
-      <div class="goods-list">
+     <!-- 结果区域 -->
+     <div class="goods-list">
         <!-- 排序 -->
         <SubSort />
         <!-- 列表 -->
+        <ul>
+          <li v-for="goods in goodsList" :key="goods.id" >
+            <GoodsItem :goods="goods" />
+          </li>
+        </ul>
+        <!-- 加载 -->
+        <XtxInfiniteLoading :loading="loading" :finished="finished" @infinite="getData"></XtxInfiniteLoading>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue-demi'
-import SubBread from './components/sub-bread.vue'
-import SubFilter from './components/sub-filter.vue'
-import SubSort from './components/sub-sort.vue'
+import SubBread from './components/sub-bread'
+import SubFilter from './components/sub-filter'
+import SubSort from './components/sub-sort'
+import GoodsItem from './components/goods-item'
+import { ref, watch } from 'vue'
+import { findSubCategoryGoods } from '@/api/category'
+import { useRoute } from 'vue-router'
 export default {
   name: 'SubCategory',
-  components: { SubBread, SubFilter, SubSort },
+  components: { SubBread, SubFilter, SubSort, GoodsItem },
   setup () {
-    const checkAll = ref(true)
+    // 1. 基础布局
+    // 2. 无限加载组件
+    // 3. 动态加载数据且渲染
+    // 4. 任何筛选条件变化需要更新列表
+    const route = useRoute()
 
-    return { checkAll }
+    // 定义数据状态 和 数据类型
+    const loading = ref(false)
+    const finished = ref(false)
+    const goodsList = ref([])
+    // 定义 api查询参数
+    let reqParams = {
+      page: 1, // 页码
+      pageSize: 20 // 每页/条数据
+    }
+    // 获取数据函数
+    const getData = () => {
+      console.log('触发了吗')
+      loading.value = true
+      reqParams.categoryId = route.params.id // 往 api查询参数 里面添加id
+      findSubCategoryGoods(reqParams).then(({ result }) => {
+        if (result.items.length) {
+          goodsList.value.push(...result.items)
+          reqParams.page++ // 页码加1
+        } else {
+          // 没有数据可以加载了
+          finished.value = true
+        }
+        loading.value = false
+      })
+    }
+    // 切换二级类目,初始化数据模板.重新获取数据
+    watch(() => route.params.id, (newVal) => {
+      if (newVal && route.path === `/category/sub/${newVal}`) {
+        // loading.value = false
+        goodsList.value = []
+        reqParams = {
+          page: 1, // 页码
+          pageSize: 20 // 每页/条数据
+        }
+        finished.value = false
+      }
+    }
+    )
+
+    return { goodsList, getData }
   }
 }
 </script>
@@ -36,5 +89,17 @@ export default {
   background: #fff;
   padding: 0 25px;
   margin-top: 25px;
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0 5px;
+    li {
+      margin-right: 20px;
+      margin-bottom: 20px;
+      &:nth-child(5n) {
+        margin-right: 0;
+      }
+    }
+  }
 }
 </style>
