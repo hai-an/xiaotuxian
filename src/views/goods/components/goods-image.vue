e7450e4f23cacc4ec19d2d5f50f70e217adcb65e
hai-an <1207783846@qq.com>
2022-11-26 14:33:08 +0800

    商品详情-图片放大镜

--- src/views/goods/components/goods-image.vue
+++ src/views/goods/components/goods-image.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="goods-image">
+    <!-- 大图片 -->
+    <div v-show="show" class="large" :style="[{backgroundImage:`url(${images[currIndex]})`},BgPosition]"></div>
     <!-- 中图 -->
-    <div class="middle">
+    <div class="middle" ref="target">
       <img :src="images[currIndex]" alt="">
+      <!-- 遮罩层 -->
+      <div v-show="show" class="layer" :style="maskPosition"></div>
     </div>
     <!-- 小图 -->
     <ul class="small">
@@ -13,7 +17,8 @@
   </div>
 </template>
 <script>
-import { ref } from 'vue'
+import { reactive, ref, watch } from 'vue'
+import { useMouseInElement } from '@vueuse/core' // 使用 该方法 拿到鼠标坐标,是否超出目标元素范围
 export default {
   name: 'GoodsImage',
   props: {
@@ -22,11 +27,55 @@ export default {
       default: () => []
     }
   },
-  setup (props) {
+  setup () {
+    // 当前预览的图片的索引
     const currIndex = ref(0)
-    return { currIndex }
+
+    // 1.是否显示遮罩层/大图片
+    const show = ref(false)
+    // 绑定中图片
+    const target = ref(null)
+    // 2.遮罩层的坐标(样式)
+    const maskPosition = reactive({
+      left: 0,
+      top: 0
+    })
+    // 3.大图片背景定位(样式)
+    const BgPosition = reactive({
+      backgroundPositionX: 0,
+      backgroundPositionY: 0
+    })
+
+    // 4.使用useMouseInElement得到基于元素左上角的坐标和是否离开元素数据
+    const { elementX, elementY, isOutside } = useMouseInElement(target)
+    // elementX 鼠标基于容器左上角X轴偏移
+    // elementY 鼠标基于容器左上角Y轴偏移
+    // isOutside 鼠标是否在模板容器外
+    watch([elementX, elementY, isOutside], () => {
+      // 5.根据得到的数据设置样式和是否显示数据
+      show.value = !isOutside.value // 预览 大图片
+      console.log('isOutside', elementX, elementY, isOutside)
+      // 定义 公共数据模板
+      const position = { x: 0, y: 0 }
+      // 控制X轴方向的定位 0-200 之间
+      if (elementX.value < 100) position.x = 0
+      else if (elementX.value > 300) position.x = 200
+      else position.x = elementX.value - 100
+      // 控制Y轴方向的定位 0-200 之间
+      if (elementY.value < 100) position.y = 0
+      else if (elementY.value > 300) position.y = 200
+      else position.y = elementY.value - 100
+
+      // 给样式赋值
+      maskPosition.left = position.x + 'px'
+      maskPosition.top = position.y + 'px'
+      BgPosition.backgroundPositionX = -2 * position.x + 'px'
+      BgPosition.backgroundPositionY = -2 * position.y + 'px'
+    })
+    return { currIndex, target, show, maskPosition, BgPosition }
   }
 }
+
 </script>
 <style scoped lang="less">
 .goods-image {
@@ -34,10 +83,32 @@ export default {
   height: 400px;
   position: relative;
   display: flex;
+  z-index: 500;
+  .large {
+    position: absolute;
+    top: 0;
+    left: 412px;
+    width: 400px;
+    height: 400px;
+    box-shadow: 0 0 10px rgba(0,0,0,0.1);
+    background-repeat: no-repeat;
+    background-size: 800px 800px;
+    background-color: #f8f8f8;
+  }
   .middle {
     width: 400px;
     height: 400px;
     background: #f5f5f5;
+    position: relative;
+    cursor: move;
+    .layer {
+      width: 200px;
+      height: 200px;
+      background: rgba(0,0,0,.2);
+      left: 0;
+      top: 0;
+      position: absolute;
+    }
   }
   .small {
     width: 80px;
