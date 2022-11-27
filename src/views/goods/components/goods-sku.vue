a4bb847ed2d01056807188c06e74e8057ebdb5d1
hai-an <1207783846@qq.com>
2022-11-27 15:17:06 +0800

    规格组件-禁用效果-设置状态

--- src/views/goods/components/goods-sku.vue
+++ src/views/goods/components/goods-sku.vue
@@ -5,14 +5,72 @@
       <dt >{{item.name}}</dt>
       <dd>
         <template v-for="val in item.values" :key="val.name">
-        <img  @click="clickSpecs(item,val)" v-if="val.picture" :class="{selected:val.selected}" :src="val.picture" :title="val.name">
-        <span :class="{selected:val.selected}" @click="clickSpecs(item,val)" v-else>{{val.name}}</span>
+        <img  @click="clickSpecs(item,val)" v-if="val.picture" :class="{selected:val.selected,disabled:val.disabled}" :src="val.picture" :title="val.name">
+        <span :class="{selected:val.selected,disabled:val.disabled}" @click="clickSpecs(item,val)" v-else>{{val.name}}</span>
       </template>
       </dd>
     </dl>
   </div>
 </template>
 <script>
+import getPowerSet from '@/vender/power-set'
+const spliter = '★'
+// 根据skus数据得到路径字典对象
+const getPathMap = (skus) => {
+  const pathMap = {}
+  // 1. 过滤出有库存有效的sku
+  skus.forEach(sku => {
+    if (sku.inventory > 0) {
+      // 计算当前有库存的sku子集
+      // 例如:[1,2,3] => [[1],[2],[3],[1,2],[1,3],[2,3],[1,2,3]]
+      // 可选值数组
+      const valueArr = sku.specs.map(val => val.valueName)
+      console.log(valueArr)
+
+      // 可选值数组 子集
+      const valueArrPowerSet = getPowerSet(valueArr)
+      console.log(valueArrPowerSet)
+
+      // 遍历子集,往pathMap插入数据
+      valueArrPowerSet.forEach(arr => {
+        // 根据arr得到字符串的key,约定key是:['蓝色','美国']
+        const key = arr.join(spliter)
+        // 给pathMap设置数据
+        if (pathMap[key]) {
+          pathMap[key].push(sku.id)
+        } else {
+          pathMap[key] = [sku.id]
+        }
+      })
+    }
+  })
+  return pathMap
+}
+// 得到当前选中规格数组
+const getSelectedArr = (specs) => {
+  const selectedArr = []
+  specs.forEach(spec => {
+    const selectVal = spec.values.find(val => val.selected)
+    selectedArr.push(selectVal ? selectVal.name : undefined)
+  })
+  return selectedArr
+}
+// 更新按钮的禁用状态
+const updateDisabledStatus = (specs, pathMap) => {
+  specs.forEach((spec, i) => {
+    const selectedArr = getSelectedArr(specs)
+    spec.values.forEach(val => {
+      // 已经选中的按钮不用判断
+      if (val.name === selectedArr[i]) return false
+      // 未选中的替换对应的值
+      selectedArr[i] = val.name
+      // 过滤无效的值得到key
+      const key = selectedArr.filter(v => v).join(spliter)
+      // 设置禁用状态
+      val.disabled = !pathMap[key]
+    })
+  })
+}
 export default {
   name: 'GoodsSku',
   props: {
@@ -22,7 +80,12 @@ export default {
     }
   },
   setup (props) {
+    const pathMap = getPathMap(props.goods.skus)
+    //  ☆ 组件初始化: 更新按钮禁用状态
+    updateDisabledStatus(props.goods.specs, pathMap)
     const clickSpecs = (item, val) => {
+      // 如果是禁用状态不作处理
+      if (val.disabled) return false
       // 1.选中时,再次点击该选项,取消选中
       // 2.当前项选中时,点击同一规格的其他 属性时,取消当前选中,给 点击目标 添加选中
       if (val.selected) {
@@ -31,8 +94,12 @@ export default {
         item.values.forEach(p => { p.selected = false })
         val.selected = true
       }
+      // ☆ 点击按钮时,更新按钮禁用状态
+      updateDisabledStatus(props.goods.specs, pathMap)
     }
-    return { clickSpecs }
+
+    console.log(pathMap)
+    return { clickSpecs, pathMap }
   }
 }
 </script>
