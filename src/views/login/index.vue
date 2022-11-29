d49002bce8868f8ea1d573073a36bec6b5610c0e
hai-an <1207783846@qq.com>
2022-11-29 21:44:24 +0800

    登录-基础布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,27 @@
 <template>
-  <div class="page-login">
-    Login
-  </div>
+  <!-- 头部 -->
+ <LoginHeader>欢迎登录</LoginHeader>
+ <!-- 主体 -->
+  <section class="login-section">
+    <div class="wrapper">
+      <nav>
+        <a href="javascript:;">账户登录</a>
+        <a href="javascript:;">扫码登录</a>
+      </nav>
+    </div>
+  </section>
+  <!-- 底部 -->
+  <LoginFooter />
 </template>
 <script>
+import LoginHeader from './components/login-header'
+import LoginFooter from './components/login-footer'
 export default {
-  name: 'PageLogin'
+  name: 'PageLogin',
+  components: {
+    LoginHeader,
+    LoginFooter
+  }
 }
 </script>
 <style scoped lang="less"></style>
