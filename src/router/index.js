76e52e3fea848f4cb4d78b0f8920cef23c86278d
hai_an <1207783846@qq.com>
2023-04-02 09:37:26 +0800

    更改路由导入方式

--- src/router/index.js
+++ src/router/index.js
@@ -1,57 +1,41 @@
 import { createRouter, createWebHashHistory, RouterView } from 'vue-router'
 import store from '@/store'
 import { h } from 'vue'
-const Layout = () => import('@/views/Layout')
-const Home = () => import('@/views/home/index')
-const TopCategory = () => import('@/views/category')
-const SubCategory = () => import('@/views/category/sub.vue')
-const Goods = () => import('@/views/goods/index')
-
-const Login = () => import('@/views/login/index') // 登录页面
-const LoginCallback = () => import('@/views/login/callback') // 登录重定向
-const Cart = () => import('@/views/cart/index')
-const PayCheckout = () => import('@/views/member/pay/checkout')
-const Pay = () => import('@/views/member/pay/index.vue')
-const PayResult = () => import('@/views/member/pay/result')
-const MemberLayout = () => import('@/views/member/Layout')
-const MemberHome = () => import('@/views/member/home')
-const MemberOrder = () => import('@/views/member/order')
-const MemberOrderDetail = () => import('@/views/member/order/detail')
 const routes = [
 // 一级路由
   {
     path: '/',
-    component: Layout,
+    component: () => import('@/views/Layout'),
     children: [
-      { path: '/', component: Home },
-      { path: '/category/:id', component: TopCategory },
-      { path: '/category/sub/:id', component: SubCategory },
-      { path: '/product/:id', component: Goods },
-      { path: '/cart', component: Cart },
-      { path: '/member/checkout', component: PayCheckout },
-      { path: '/member/pay', component: Pay },
-      { path: '/pay/callback', component: PayResult },
+      { path: '/', component: () => import('@/views/home/index.vue') },
+      { path: '/category/:id', component: () => import('@/views/category/index.vue') },
+      { path: '/category/sub/:id', component: () => import('@/views/category/sub.vue') },
+      { path: '/product/:id', component: () => import('@/views/goods/index.vue') },
+      { path: '/cart', component: () => import('@/views/cart/index.vue') },
+      { path: '/member/checkout', component: () => import('@/views/member/pay/checkout.vue') },
+      { path: '/member/pay', component: () => import('@/views/member/pay/index.vue') },
+      { path: '/pay/callback', component: () => import('@/views/member/pay/result.vue') },
       {
         path: '/member',
-        component: MemberLayout,
+        component: () => import('@/views/member/Layout.vue'),
         children: [
-          { path: '/member', component: MemberHome }, // 个人中心
+          { path: '/member', component: () => import('@/views/member/home/index.vue') }, // 个人中心
           // { path: '/member/order', component: MemberOrder }
           {
             path: '/member/order', // 个人订单
             // vue3.0 需要有嵌套关系才能模糊匹配
             component: { render: () => h(RouterView) },
             children: [
-              { path: '', component: MemberOrder },
-              { path: ':id', component: MemberOrderDetail }
+              { path: '', component: () => import('@/views/member/order/index.vue') },
+              { path: ':id', component: () => import('@/views/member/order/detail.vue') }
             ]
           }
         ]
       }
     ]
   },
-  { path: '/login', component: Login }, // 登录页面
-  { path: '/login/callback', component: LoginCallback } // 登录重定向
+  { path: '/login', component: () => import('@/views/login/index') }, // 登录页面
+  { path: '/login/callback', component: () => import('@/views/login/callback') } // 登录重定向
 
 ]
 
